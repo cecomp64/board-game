88b9c285096a6cfe72fe6f69f54f19bf977c6526
Carl Svensson <csvenss2@gmail.com>
2015-08-26 23:31:32 -0700

    ...

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,7 +85,7 @@ Rails.application.configure do
         :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
         :url => ':s3_domain_url',
         :path => '/:class/:attachment/:id_partition/:style/:filename',
-        :s3_host_name => 's3-us-west-2.amazonaws.com'
+        :s3_host_name => 's3-us-west-1.amazonaws.com'
       }
   }
 end
